0f3574b2bdb8a261ce33b9d078274e2e73215cee
若海 <wang@rehiy.com>
2023-12-27 20:52:26 +0800

    refactor: 将文件移入apps

--- apps/main.js
+++ apps/main.js
@@ -1,11 +1,11 @@
 import { config } from 'dotenv';
 
-import { serve } from '../library/server.js';
-import { preload } from '../library/wechat.js';
+import { serve } from './library/server.js';
+import { preload } from './library/wechat.js';
 
-import '../events/message_ai.js';
-import '../events/message_print.js';
-import '../events/message_system.js';
+import './events/message_ai.js';
+import './events/message_print.js';
+import './events/message_system.js';
 
 export default async () => {
 
