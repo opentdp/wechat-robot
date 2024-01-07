ef1415664c3a0aed73a19adadf89313b407d0b9e
若海 <wang@rehiy.com>
2024-01-07 13:11:23 +0800

    feat: 个人对话也需要唤醒词

--- apps/events/message_ai.js
+++ apps/events/message_ai.js
@@ -10,17 +10,16 @@ emitter.on('message', async data => {
             if (/^gh_/.test(data.sender)) {
                 return;
             }
-            // 处理私聊消息
-            if (!data.is_group) {
-                const resp = await aichat(data.sender, data.content);
-                wechat.sendTxt(data.sender, resp);
-                return;
-            }
-            // 处理群内的指令
+            // 处理指令消息
             if (/^\/(ai\s|[a-z:0-9]{2,30}$)/.test(data.content)) {
-                const name = await wechat.getUserName(data.sender);
-                const resp = await aichat(data.sender, data.content, data.roomid);
-                wechat.sendTxt(data.roomid, '@' + name + '\n' + resp, data.sender);
+                if (data.is_group) {
+                    const name = await wechat.getUserName(data.sender);
+                    const resp = await aichat(data.sender, data.content, data.roomid);
+                    wechat.sendTxt(data.roomid, '@' + name + '\n' + resp, data.sender);
+                } else {
+                    const resp = await aichat(data.sender, data.content);
+                    wechat.sendTxt(data.sender, resp);
+                }
                 return;
             }
             break;
