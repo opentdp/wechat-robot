bcf9e38df3471b18ac011e31b709583a9c79c0b6
若海 <wang@rehiy.com>
2024-01-09 09:03:53 +0800

    feat: 适配新的接口

--- apps/events/message_system.js
+++ apps/events/message_system.js
@@ -6,9 +6,9 @@ emitter.on('message', async data => {
     switch (data.type) {
         case 37:
             // 自动接受好友请求
-            const content = data.content;
-            if (content && content.msg && content.msg.$) {
-                wechat.accpetFriend(content.msg.$.scene, content.msg.$.encryptusername, content.msg.$.ticket)
+            if (data.content && data.content.msg) {
+                const msg = data.content.msg;
+                wechat.accpetFriend(msg.scene, msg.encryptusername, msg.ticket)
             }
             break;
         case 10000:
@@ -19,7 +19,7 @@ emitter.on('message', async data => {
             }
             // 添加好友后自动回复
             if (/现在可以开始聊天了/.test(data.content)) {
-                wechat.sendTxt(data.sender, '你好，我是小欧，可以和我聊天啦！回复 /help 查看帮助信息。');
+                wechat.sendTxt(data.sender, '你好，我是小欧，回复 /help 查询指令');
             }
             break;
     }
